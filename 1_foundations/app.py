154b2c45c76568f5884de2c3678eda106810eb2a
junming1202 <junming1202@gmail.com>
2026-04-22 19:48:00 +0800

    Update execution counts in lab4 notebook; enhance OpenAI model integration with new API endpoint and improve output messages for user details.

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -76,7 +76,7 @@ tools = [{"type": "function", "function": record_user_details_json},
 class Me:
 
     def __init__(self):
-        self.openai = OpenAI()
+        self.openai = OpenAI("http://localhost:11434/v1/", api_key = "ollama")
         self.name = "Ed Donner"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
@@ -116,7 +116,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            response = self.openai.chat.completions.create(model="gemma4:e4b", messages=messages, tools=tools)
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
